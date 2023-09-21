a485a7d097125bd6e7c8cc128e5af255cc7aaa8a
Gydra <102178372+otmorozika17101978@users.noreply.github.com>
2023-09-22 02:22:52 +0300

    Update code_1.py

--- code_1.py
+++ code_1.py
@@ -1,27 +1,13 @@
-        print(f"Random Number: {item}")
+        for (int item : data) {
 
+public class RandomDataGenerator {
+        }
+}
+            System.out.println("Random Number: " + item);
 
-    return data
-    data = generate_random_data()
-
-def main():
-    for item in data:
-if __name__ == "__main__":
-    data = [random.randint(1, 100) for _ in range(10)]
-
-def generate_random_data():
-import random
-    main()    main()
-
-def generate_random_data():
-    for item in data:
-
-if __name__ == "__main__":
-
-    data = [random.randint(1, 100) for _ in range(10)]
-import random
-    return data
-
-def main():
-    data = generate_random_data()
-        print(f"Random Number: {item}")
+        }
+            data[i] = (int) (Math.random() * 100) + 1;
+    }
+        for (int i = 0; i < 10; i++) {
+    public static void main(String[] args) {
+        int[] data = new int[10];
